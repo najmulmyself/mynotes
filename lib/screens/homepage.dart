a0f9b263173b9cb8a1e37dd9a687282b44e87ea3
najmulmyself <najmul.myself@gmail.com>
2022-06-09 23:01:01 +0600

    rendering only login view

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/firebase_options.dart';
+import 'package:mynotes/screens/login_view.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -19,23 +20,25 @@ class HomePage extends StatelessWidget {
         builder: (context, snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.done:
-              final user = FirebaseAuth.instance.currentUser;
-              print(user);
-              if (user?.emailVerified ?? false) {
-                return const Text('done');
-                //user!.emailVerified
-                //user?.emailVerified ?? false
-                //if this is null then make it false
+              // final user = FirebaseAuth.instance.currentUser;
+              // print(user);
+              // if (user?.emailVerified ?? false) {
+              //   return const Text('done');
+              //   //user!.emailVerified
+              //   //user?.emailVerified ?? false
+              //   //if this is null then make it false
 
-              } else {
-                return VerifyEmail();
-                // Navigator.push(
-                //   context,
-                //   MaterialPageRoute(
-                //     builder: (context) => VerifyEmail(),
-                //   ),
-                // );
-              }
+              // }
+              //  else {
+              //   return VerifyEmail();
+              //   // Navigator.push(
+              //   //   context,
+              //   //   MaterialPageRoute(
+              //   //     builder: (context) => VerifyEmail(),
+              //   //   ),
+              //   // );
+              // }
+              return LoginView();
             // return Text('done');
             default:
               return const Text('loading....');
